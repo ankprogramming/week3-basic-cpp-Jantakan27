952f9d723930f5aed760bcb14ae5aac7c23cb664
Unknown <jantakan27b@gmail.com>
2017-11-27 18:13:50 +0700

    แก้ไขข้อที่ 1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
-  cout << "Mr. Anukul" << endl;
+  cout << "Miss. Jantakan Nakkullabut" << endl;
+  cout << "Nickname Bam" << endl;
+  cout << "Sex Female" << endl;
+  cout << "Birthday November 27,1998" << endl;
+  cout << "Single" << endl;
   return 0;
 }
